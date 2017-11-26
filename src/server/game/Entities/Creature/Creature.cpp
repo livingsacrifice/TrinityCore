ac97424ecbaa9b8b2ee8ad5e3576d7872513b5c4
Shauren <shauren.trinity@gmail.com>
2017-11-26 16:52:42 +0100

    Core/Creatures: Remove hover on death

--- src/server/game/Entities/Creature/Creature.cpp
+++ src/server/game/Entities/Creature/Creature.cpp
@@ -1940,7 +1940,10 @@ void Creature::setDeathState(DeathState s)
         if (m_formation && m_formation->getLeader() == this)
             m_formation->FormationReset(true);
 
-        if ((CanFly() || IsFlying()))
+        bool needsFalling = IsFlying() || IsHovering();
+        SetHover(false);
+
+        if (needsFalling)
             GetMotionMaster()->MoveFall();
 
         Unit::setDeathState(CORPSE);
@@ -2985,7 +2988,7 @@ void Creature::UpdateMovementFlags()
     {
         SetCanFly(false);
         SetDisableGravity(false);
-        if (CanHover() || HasAuraType(SPELL_AURA_HOVER))
+        if (IsAlive() && (CanHover() || HasAuraType(SPELL_AURA_HOVER)))
             SetHover(true);
     }
 
